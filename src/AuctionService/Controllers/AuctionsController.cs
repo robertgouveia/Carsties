6e7f3a9a794f41b78a257a045a43b5f8dad46fe1
websitech <robertgouveia03@hotmail.com>
2024-12-10 18:59:00 +0000

    End of section 2

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,95 @@
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _auctionDbContext;
+    private readonly IMapper _mapper;
+    
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _auctionDbContext = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _auctionDbContext.Auctions
+            .Include(x => x.Item)
+            .OrderBy(x => x.Item.Make)
+            .ToListAsync();
+        
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _auctionDbContext.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id.Equals(id));
+        
+        if (auction == null) return NotFound();
+        
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuction)
+    {
+        var auction = _mapper.Map<Auction>(createAuction);
+        
+        // TODO: Add current user as seller
+        auction.Seller = "test";
+        
+        _auctionDbContext.Auctions.Add(auction);
+        var result = await _auctionDbContext.SaveChangesAsync() > 0;
+        
+        if (!result) return BadRequest("Could not create auction");
+
+        return CreatedAtAction(nameof(GetAuctionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAuction(Guid id, UpdateAuctionDto updateAuction)
+    {
+        var auction = await _auctionDbContext.Auctions
+            .Include(x => x.Item)
+            .FirstOrDefaultAsync(x => x.Id.Equals(id));
+        
+        if (auction == null) return NotFound();
+
+        // TODO: Check Seller's Username
+        
+        auction.Item.Make =  updateAuction.Make ?? auction.Item.Make;
+        auction.Item.Color = updateAuction.Color ?? auction.Item.Color;
+        auction.Item.Mileage = updateAuction.Mileage ?? auction.Item.Mileage;
+        auction.Item.Model = updateAuction.Model ?? auction.Item.Model;
+        auction.Item.Year = updateAuction.Year ?? auction.Item.Year;
+      
+        return await _auctionDbContext.SaveChangesAsync() < 0 ? BadRequest("Could not update auction") : Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _auctionDbContext.Auctions.FindAsync(id);
+        
+        if (auction == null) return NotFound();
+        
+        // TODO: Check Seller's Username
+        
+        _auctionDbContext.Auctions.Remove(auction);
+        return await _auctionDbContext.SaveChangesAsync() < 0 ? BadRequest("Could not delete auction") : Ok();
+    }
+    
+}
---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,29 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // Assembly required for reading classes available
+
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+
+app.Run();
